09480d85f8ce55dfb14e77996432c926a409b110
ftnext <takuyafjp+develop@gmail.com>
2021-09-13 23:58:11 +0900

    [feat] Migrate from talks.searcher
    https://github.com/pyconjp/talks.searcher.2021/blob/2c09bebf64251b3b24819a9cc9ab4125ffc6ddcd/app.py

---
+++ pyconjp_domains/talks.py
@@ -0,0 +1,160 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from dataclasses import dataclass
+
+
+def get_single_choice_category_value(category: dict) -> str:
+    """sessionizeのAPIの返り値から単一選択項目の選択値を返す"""
+    return category["categoryItems"][0]["name"]
+
+
+@dataclass
+class Speaker:
+    name: str
+
+
+@dataclass
+class Category:
+    track: str
+    level: str
+    speaking_language: str
+    slide_language: str
+
+    @staticmethod
+    def flatten_raw_json(categories: list[dict]) -> dict[str, str]:
+        """sessionizeのAPIの返り値のうち、ネストしたcategoriesフィールドをパースして平坦にする"""
+        for category in categories:
+            if category["name"] == "Track":
+                track = get_single_choice_category_value(category)
+                continue
+            if category["name"] == "Level":
+                level = get_single_choice_category_value(category)
+                continue
+            if category["name"] == "Language":
+                speaking_language = get_single_choice_category_value(category)
+                continue
+            if (
+                category["name"]
+                == "発表資料の言語 / Language of presentation material"
+            ):
+                slide_language = get_single_choice_category_value(category)
+                continue
+        return {
+            "track": track,
+            "level": level,
+            "speaking_language": speaking_language,
+            "slide_language": slide_language,
+        }
+
+    @classmethod
+    def from_raw_json(cls, categories):
+        return cls(**cls.flatten_raw_json(categories))
+
+
+@dataclass
+class QuestionAnswer:
+    elevator_pitch: str
+    audience_prior_knowledge: str
+    audience_take_away: str
+
+    @staticmethod
+    def flatten_raw_json(question_answers: list[dict]) -> dict[str, str]:
+        for qa in question_answers:
+            if qa["question"] == "Elevator Pitch":
+                elevator_pitch = qa["answer"]
+                continue
+            if qa["question"] == "オーディエンスに求める前提知識":
+                audience_prior_knowledge = qa["answer"]
+                continue
+            if qa["question"] == "オーディエンスが持って帰れる具体的な知識やノウハウ":
+                audience_take_away = qa["answer"]
+                continue
+        return {
+            "elevator_pitch": elevator_pitch,
+            "audience_prior_knowledge": audience_prior_knowledge,
+            "audience_take_away": audience_take_away,
+        }
+
+    @classmethod
+    def from_raw_json(cls, question_answers):
+        return cls(**cls.flatten_raw_json(question_answers))
+
+
+@dataclass
+class Talk:
+    id: int
+    title: str
+    description: str
+    category: Category
+    answer: QuestionAnswer
+    speakers: list[Speaker]
+
+
+@dataclass
+class Talks(Sequence):
+    talks: list[Talk]
+
+    def __len__(self):
+        return len(self.talks)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return self.__class__(self.talks[key])
+        return self.talks[key]
+
+    @classmethod
+    def from_raw_json(cls, data):
+        talks = []
+        for session in data:
+            speakers = [
+                Speaker(speaker["name"]) for speaker in session["speakers"]
+            ]
+            category = Category.from_raw_json(session["categories"])
+            question_answers = QuestionAnswer.from_raw_json(
+                session["questionAnswers"]
+            )
+            talk = Talk(
+                int(session["id"]),
+                session["title"],
+                session["description"],
+                category,
+                question_answers,
+                speakers,
+            )
+            talks.append(talk)
+        return cls(sorted(talks, key=lambda t: t.id))
+
+    def filter_by(self, request: dict) -> "Talks":
+        talks = self.talks
+        if request["tracks"]:
+            talks = [t for t in talks if t.category.track in request["tracks"]]
+        if request["levels"]:
+            talks = [t for t in talks if t.category.level in request["levels"]]
+        if request["keywords"]:
+            # AND検索。case-insensitive。
+            # タイトル・エレベータピッチ・前提知識・持ち帰れるものの部分文字列か（詳細は使っていない）
+            for keyword in request["keywords"]:
+                talks = [
+                    t
+                    for t in talks
+                    if keyword
+                    in "\n".join(
+                        [
+                            t.title,
+                            t.answer.elevator_pitch,
+                            t.answer.audience_prior_knowledge,
+                            t.answer.audience_take_away,
+                        ]
+                    ).lower()
+                ]
+        if request["is_english_only"]:
+            talks = [
+                t
+                for t in talks
+                # speaking_language は Japanese / English
+                if t.category.speaking_language == "English"
+                # slide_language は Japanese only / English only / Both
+                or t.category.slide_language != "Japanese only"
+            ]
+        return self.__class__(talks)
