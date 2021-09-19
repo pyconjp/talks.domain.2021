93e23f7e62fde6ecd9cb5273f447db279d869fc3
ftnext <takuyafjp+develop@gmail.com>
2021-09-19 15:00:13 +0900

    [feat] Implement room/speaker map factories

--- pyconjp_domains/core.py
+++ pyconjp_domains/core.py
@@ -1,6 +1,8 @@
 import json
 from urllib.request import urlopen
 
+from pyconjp_domains.talks import Speaker
+
 
 def fetch_data(url):
     with urlopen(url) as res:
@@ -18,3 +20,11 @@ def is_included(title):
 def filter_sessions(sessions):
     """タイムテーブルに載せるsessionだけに絞り込む"""
     return filter(lambda d: is_included(d["title"]), sessions)
+
+
+def create_room_id_name_map(room_data):
+    return {d["id"]: d["name"] for d in room_data}
+
+
+def create_speaker_id_map(speaker_data):
+    return {d["id"]: Speaker(d["fullName"], d["bio"]) for d in speaker_data}
