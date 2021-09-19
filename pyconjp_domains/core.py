46a5a1adf7270920e9928c1aa8e9ce5212c7fb57
ftnext <takuyafjp+develop@gmail.com>
2021-09-19 14:48:21 +0900

    [feat] Implement core.filter_sessions

--- pyconjp_domains/core.py
+++ pyconjp_domains/core.py
@@ -5,3 +5,16 @@ from urllib.request import urlopen
 def fetch_data(url):
     with urlopen(url) as res:
         return json.load(res)
+
+
+def is_included(title):
+    patterns = ["Venue open", "スペシャルブース紹介", "Ask the speaker", "Break"]
+    for pattern in patterns:
+        if title.startswith(pattern):
+            return False
+    return True
+
+
+def filter_sessions(sessions):
+    """タイムテーブルに載せるsessionだけに絞り込む"""
+    return filter(lambda d: is_included(d["title"]), sessions)
