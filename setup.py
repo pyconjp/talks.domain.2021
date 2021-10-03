9d3007c6416c567f033753c5ba63d50a68d45e31
ftnext <takuyafjp+develop@gmail.com>
2021-10-03 14:15:28 +0900

    [chore] Bump up with enhancement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyconjp_domains",
-    version="0.0.2",
+    version="0.1.0",
     packages=find_packages(exclude=["tests.*", "tests"]),
     python_requires=">=3.7",
     classifiers=[
