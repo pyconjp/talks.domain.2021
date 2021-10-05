1b0e3bf0b1fed99da2d5c1a0b90067fb841ca3cf
ftnext <takuyafjp+develop@gmail.com>
2021-10-05 14:47:09 +0900

    [chore] Bump up supporting level All

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyconjp_domains",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests.*", "tests"]),
     python_requires=">=3.7",
     classifiers=[
