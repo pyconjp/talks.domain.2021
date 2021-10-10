598b542e46ce86d58c1b7cc3a2691d4ba462d85d
ftnext <takuyafjp+develop@gmail.com>
2021-10-10 19:19:07 +0900

    [chore] Bump up with --fields argument

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyconjp_domains",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(exclude=["tests.*", "tests"]),
     python_requires=">=3.7",
     classifiers=[
