e4c41af405e79ecffb0d324293906cbc5f9c37c0
ftnext <takuyafjp+develop@gmail.com>
2021-10-10 18:43:51 +0900

    [feat] Add fields argument with default value

--- pyconjp_domains/__main__.py
+++ pyconjp_domains/__main__.py
@@ -4,6 +4,29 @@ import os
 
 from pyconjp_domains.core import fetch_talks
 
+WEBSITE_TIMETABLE_FIELDS = [
+    "id",
+    "title",
+    "room",
+    "day",
+    "start_time",
+    "slot_number AS no",
+    "elevator_pitch",
+    "prior_knowledge AS prerequisite_knowledge",
+    "take_away AS audience_takeaway",
+    "level AS audience_python_level",
+    "track",
+    "speaking_language AS lang_of_talk",
+    "slide_language AS lang_of_slide",
+    "description",
+    "duration_min",
+    "slide_url",
+    "recording_url",
+    # 2021は複数人での発表がないため
+    "speaker_names AS name",
+    "speaker_profiles AS profile",
+]
+
 
 def parse_field_arguments(arguments):
     fields, headers = (), ()
@@ -18,32 +41,13 @@ def parse_field_arguments(arguments):
     return fields, headers
 
 
-def retrieve_talks_in_timetable(output):
+def retrieve_talks_in_timetable(output, field_arguments):
     endpoint_id = os.environ["ENDPOINT_ID"]
     url = f"https://sessionize.com/api/v2/{endpoint_id}/view/All"
     talks = fetch_talks(url)
 
-    fields = [
-        "id",
-        "title",
-        "room",
-        "day",
-        "start_time",
-        "slot_number",
-        "elevator_pitch",
-        "prior_knowledge",
-        "take_away",
-        "level",
-        "track",
-        "speaking_language",
-        "slide_language",
-        "description",
-        "duration_min",
-        "slide_url",
-        "recording_url",
-        "speaker_names",
-        "speaker_profiles",
-    ]
+    fields, headers = parse_field_arguments(field_arguments)
+
     rows = [talk.as_list(fields) for talk in talks.sorted()]
 
     for row in rows:
@@ -54,27 +58,6 @@ def retrieve_talks_in_timetable(output):
         # Noneはjoinできないので、空文字列に変換する
         row[-1] = "\n\n".join(v or "" for v in row[-1])
 
-    headers = (
-        "id",
-        "title",
-        "room",
-        "day",
-        "start_time",
-        "no",
-        "elevator_pitch",
-        "prerequisite_knowledge",
-        "audience_takeaway",
-        "audience_python_level",
-        "track",
-        "lang_of_talk",
-        "lang_of_slide",
-        "description",
-        "duration_min",
-        "slide_url",
-        "recording_url",
-        "name",
-        "profile",
-    )
     with open(output, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerows([headers] + rows)
@@ -86,7 +69,10 @@ if __name__ == "__main__":
     )
     parser.add_argument("data_type", choices=("timetable",))
     parser.add_argument("output_csv")
+    parser.add_argument(
+        "--fields", nargs="*", default=WEBSITE_TIMETABLE_FIELDS
+    )
     args = parser.parse_args()
 
     if args.data_type == "timetable":
-        retrieve_talks_in_timetable(args.output_csv)
+        retrieve_talks_in_timetable(args.output_csv, args.fields)
