5d3ce31e4c5336a1479ae29f2c93543eaa5f29ff
ftnext <takuyafjp+develop@gmail.com>
2021-10-17 14:53:29 +0900

    [style] Tweak import (same as other fixtures)

--- tests/pyconjp_domains/fixtures/factories__speaker_factory.py
+++ tests/pyconjp_domains/fixtures/factories__speaker_factory.py
@@ -1,4 +1,4 @@
-import pyconjp_domains.talks as t
+from pyconjp_domains.talks import Speaker
 
 id_to_raw_data_map = {
     "3be1e664-95bd-4bf0-bd51-6a642c643379": {
@@ -14,7 +14,7 @@ id_to_raw_data_map = {
 }
 
 create_target_id = "186683ea-3f6d-4f95-b392-14cbf70758a1"
-create_expected = t.Speaker("スピーカー2", "スピーカー2の輝かしい経歴")
+create_expected = Speaker("スピーカー2", "スピーカー2の輝かしい経歴")
 
 speakers_raw_data = [
     {
