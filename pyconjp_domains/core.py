393b43da9bb479e803df4ca5d026c86726a5a6c1
ftnext <takuyafjp+develop@gmail.com>
2021-10-17 16:46:36 +0900

    [refactor] Core does not need to how to create talk by facotry

--- pyconjp_domains/core.py
+++ pyconjp_domains/core.py
@@ -1,15 +1,14 @@
 import json
-from datetime import datetime
 from urllib.request import urlopen
 
-from pyconjp_domains.constants import SESSIONIZE_DATETIME_FORMAT
 from pyconjp_domains.factories import (
     CategoryFactory,
     QuestionAnswerFactory,
+    ScheduledTalkFactory,
     SlotFactory,
     SpeakerFactory,
 )
-from pyconjp_domains.talks import ScheduledTalk, ScheduledTalks
+from pyconjp_domains.talks import ScheduledTalks
 
 
 def fetch_data(url):
@@ -39,59 +38,26 @@ def _filter_with_modal_sessions(sessions):
             yield session
 
 
-def calculate_duration_min(start: str, end: str) -> int:
-    start_datetime = datetime.strptime(start, SESSIONIZE_DATETIME_FORMAT)
-    end_datetime = datetime.strptime(end, SESSIONIZE_DATETIME_FORMAT)
-    duration = end_datetime - start_datetime
-    return duration.seconds // 60
-
-
 def create_talks_from_data(data):
-    speaker_factory = SpeakerFactory.from_(data["speakers"])
+    sessions = list(filter_sessions(data["sessions"]))
+
     category_factory = CategoryFactory.from_(data["categories"])
     question_answer_factory = QuestionAnswerFactory.from_(data["questions"])
-
-    sessions = list(filter_sessions(data["sessions"]))
+    speaker_factory = SpeakerFactory.from_(data["speakers"])
     slot_factory = SlotFactory.from_(
         data["rooms"],
         set(s["startsAt"] for s in _filter_with_modal_sessions(sessions)),
     )
+    talk_factory = ScheduledTalkFactory(
+        category_factory,
+        question_answer_factory,
+        speaker_factory,
+        slot_factory,
+    )
+
     talks = []
     for session in sessions:
-        slot = slot_factory.create(session["startsAt"], session["roomId"])
-        duration_min = calculate_duration_min(
-            session["startsAt"], session["endsAt"]
-        )
-        if session["isServiceSession"]:
-            talk = ScheduledTalk(
-                session["id"],
-                session["title"],
-                session["description"],
-                None,
-                None,
-                [],
-                slot,
-                duration_min,
-            )
-        else:
-            talk = ScheduledTalk(
-                session["id"],
-                session["title"],
-                session["description"],
-                category_factory.create(
-                    session["categoryItems"], session["isPlenumSession"]
-                ),
-                question_answer_factory.create(session["questionAnswers"]),
-                [
-                    speaker_factory.create(speaker_id)
-                    for speaker_id in session["speakers"]
-                ],
-                slot,
-                duration_min,
-                # Workaround: Use live URL as slide URL
-                session["liveUrl"],
-                session["recordingUrl"],
-            )
+        talk = talk_factory.create(session)
         talks.append(talk)
 
     return ScheduledTalks(talks)
